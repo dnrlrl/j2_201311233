71db1caeee3300798dfa27504ab032cf50af2781
dnrlrl <leewwo2o@naver.com>
2017-04-19 00:00:22 +0900

    Add files via upload

---
+++ src/headfirst/factory/pizzas/PotatoPizza.java
@@ -0,0 +1,10 @@
+package headfirst.factory.pizzas;
+
+public class PotatoPizza extends Pizza{
+  public PotatoPizza(){
+    name="PotatoPizza";
+    dough="Original crust";
+    sauce="Hot sauce";
+    toppings.add("Tomato");
+  }
+}
